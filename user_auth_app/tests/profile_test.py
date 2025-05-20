4e06c1fddace6e3a398dd13d655f95c140a6e3f4
phischa <ph.git@mailbox.org>
2025-05-20 22:05:21 +0200

    test files

---
+++ user_auth_app/tests/profile_test.py
@@ -0,0 +1,5 @@
+from django.urls import reverse
+from datetime import datetime
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+from rest_framework.authtoken.models import Token 
